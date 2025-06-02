5d1ce1890848998a5c26ebb0ce7f6e6a6e8e9339
Aloberto <aloberto@gmail.com>
2025-06-02 13:35:02 -0500

    Changes in manage.py

--- backend/manage.py
+++ backend/manage.py
@@ -14,7 +14,7 @@ import sys
 from django.core.management import execute_from_command_line
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
     execute_from_command_line(sys.argv)
 
 if __name__ == '__main__':
